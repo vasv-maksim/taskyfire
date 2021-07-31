33038486822bf5dee99f1d0f35ae5f597fda2686
vasev maksim <vasvmaksim@gmail.com>
2021-07-31 18:44:15 +0300

    Добавил ngrx

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from 'src/environments/environment';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -11,6 +14,11 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    StoreModule.forRoot({}),
+    !environment.production ? StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }) : [],
   ],
   providers: [],
   bootstrap: [AppComponent],
