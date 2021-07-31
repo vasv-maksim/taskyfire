48623ae5d23f3305b275d224e13059e753cf8ea4
vasev maksim <vasvmaksim@gmail.com>
2021-07-31 10:55:01 +0300

    Добавил eslint, husky, lint-stage

--- index.js
+++ index.js
@@ -7,21 +7,20 @@ async function main() {
     app.get('/', (req, res) => {
       res.status(200).json({
         message: 'Success',
-      })
-    })
+      });
+    });
     console.log('Do something');
-  }
-  catch (e) {
+  } catch (e) {
     console.log('Error:', e.message);
     process.exit(1);
   }
-};
+}
 
 if (process.env.NODE_ENV === 'prod') {
   console.log('Do something as prod');
-};
+}
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => { console.log(`Server run on port ${PORT}...`) });
+app.listen(PORT, () => { console.log(`Server run on port ${PORT}...`); });
 
 main();
