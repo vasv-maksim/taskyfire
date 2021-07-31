3427e7804474d588ce1999c979f4d4a0541db72e
vasev maksim <vasvmaksim@gmail.com>
2021-07-31 09:46:34 +0300

    Добавил функцию main

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({
-    message: 'Success',
-  })
-})
+async function main() {
+  try {
+    console.log('Do something');
+  }
+  catch (e) {
+    console.log('Error:', e.message);
+    process.exit(1);
+  }
+};
+
+if (process.env.NODE_ENV === 'prod') {
+  console.log('Do something as prod');
+};
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => { console.log(`Server run on port ${PORT}...`) });
+
+main();
