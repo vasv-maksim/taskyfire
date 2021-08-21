6a60b792229eb6d2d9beb2c7a3bbee9d2f197606
vasev maksim <vasvmaksim@gmail.com>
2021-08-21 12:39:31 +0300

    Добавил связанные группы списков

--- client/src/app/kanban-module/kanban.module.ts
+++ client/src/app/kanban-module/kanban.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 
 import { KanbanComponent } from './kanban/kanban.component';
 import { SharedModule } from '../shared-module/shared.module';
-import { ColumnComponent } from './kanban/column/column.component';
 
 @NgModule({
   declarations: [
     KanbanComponent,
-    ColumnComponent,
   ],
   imports: [
     SharedModule,
--- client/src/app/kanban-module/kanban/kanban.component.ts
+++ client/src/app/kanban-module/kanban/kanban.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'tkr-kanban',
@@ -6,11 +7,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./kanban.component.scss'],
 })
 export class KanbanComponent {
-  public columns: string[] = [
-    'Backlog',
-    'SelectedForDev',
-    'InProgress',
-    'Review',
-    'Done',
+  public columns = [
+    {
+      id: '1',
+      title: 'Backlog',
+      tasks: [{
+        name: 'Item 1',
+      },
+      {
+        name: 'Item 2',
+      },
+      {
+        name: 'Item 3',
+      },
+      {
+        name: 'Item 4',
+      }],
+    },
+    {
+      id: '2',
+      title: 'SelectedForDev',
+      tasks: [],
+    },
+    {
+      id: '3',
+      title: 'InProgress',
+      tasks: [],
+    },
+    {
+      id: '4',
+      title: 'Review',
+      tasks: [],
+    },
+    {
+      id: '5',
+      title: 'Done',
+      tasks: [],
+    },
   ];
+
+  public dropTask(event: CdkDragDrop<string[]>): void {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+  }
+
+  public getConnectedList(): any[] {
+    return this.columns.map((x) => x.id);
+  }
 }
